c8d50221c88ab7c85d3b605b4736eb56842e429f
Dariush Molavi <dariush.molavi@L-3Com.com>
2005-08-04 12:25:34 +0000

    2005-08-04      dmolavi  <dari@nukedgallery.net>         NEW:           * German language file         FIX:           * Changelog format
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/integration@11171 57fcd75e-5312-0410-8df3-f5eb6fbb1595

---
+++ gallery2/phpnuke/html/modules/gallery2/admin/language/lang-german.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * $RCSfile$
+ *
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2005 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+/**
+ * Gallery 2 German language file for PHPNuke.
+ * @version $Revision$ $Date$
+ * @author Dariush Molavi <dari@nukedgallery.net>
+ * @translator Christoph Klempau / Jens Tkotz
+ */
+
+
+define("MOD_NAME","gallery2");
+
+define("_PHPEMBEDFILE","Absoluter, vollständiger Pfad zu embed.php im Gallery2 Verzeichnis");
+define("_EMBEDURI","URL zur Gallery 2");
+define("_RELATIVEG2PATH","Relativer Pfad zum Gallery 2 Verzeichnis");
+define("_LOGINREDIRECT","PHP-Nuke Login Umleitung");
+define("_ACTIVEUSERID","Aktive User ID");
+define("_UPDATEMAINSETTINGSG2","Konfig G2 speichern");
+define("_G2USEREXPORT","Benutzer exportieren");
+define("_G2_ERROR","Fehler");
+define("_NOTWRITABLE","ist nicht beschreibbar");
+define("_CFG_UPDATED","Konfigurationsdatei aktualisiert");
+define("_USER_EXPORT_UPDATED","Die Benutzer sind nun synchronisiert");
+define("_USER_EXPORT_FAILED","Der Export der Benutzer schlug fehl");
+define("_USER_EXPORT_COMPLETED","Benutzexport erfolgreich abgeschlossen");
+define("_G2_NEXT_PAGE","Zur nächsten Seite");
+define("_PHPEMBEDFILE_ERROR","Sie müssen zuerst die Gallery2 installieren und konfigurieren. Anschließend geben Sie in dieses Feld den Gallery2 Hauptordner ein.");
+define("_UPDATEEMBEDSETTINGSG2","Gallery Installation prüfen\nund Integrationseinstellungen aktualisieren");
+define("_G2_EMBED_PHP_FILE","embed.php");
+define("_G2_CONFIGURATION_NOT_DONE","Das Module wurde noch nicht vollständig konfiguriert.");
+
+?>
