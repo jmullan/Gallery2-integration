d706fbb6085876bae860f1001f3569c536e2f5b7
Dariush Molavi <dariush.molavi@L-3Com.com>
2005-08-30 17:37:42 +0000

    UPDATE:   * Added Spanish language files (translated by     Diego Vego).
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/integration@11509 57fcd75e-5312-0410-8df3-f5eb6fbb1595

---
+++ gallery2/phpnuke/html/modules/gallery2/admin/language/lang-spanish.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * $RCSfile$
+ *
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2005 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+/**
+ * Gallery 2 language file for PHPNuke.
+ * @version $Revision$ $Date$
+ * @translator Diego Vego
+ * @author Dariush Molavi <dari@nukedgallery.net>
+ */
+
+
+define("MOD_NAME","gallery2");
+
+define("_PHPEMBEDFILE","Ruta completa de acceso a la carpeta de Gallery2");
+define("_EMBEDURI","URL de anidación de Gallery");
+define("_RELATIVEG2PATH","Ruta relativa al diretorio de Gallery2");
+define("_LOGINREDIRECT","URL para login de usuarios");
+define("_ACTIVEUSERID","ID de Usuarios");
+define("_UPDATEEMBEDSETTINGSG2","Verfique los valores valores\nde instalación e integración de\nGallery2. Proceda a Actualizar");
+define("_UPDATEMAINSETTINGSG2","Actualizar la\nconfiguración principal");
+define("_G2USEREXPORT","Exportar los usuarios de\nPhpNuke a Gallery 2");
+define("_G2_ERROR","Error");
+define("_NOTWRITABLE","no está permitida la escritura en el archivo");
+define("_CFG_UPDATED","Archivo Config Actualizado");
+define("_USER_EXPORT_UPDATED","La lista de usuarios ahora esta sincronizada");
+define("_USER_EXPORT_FAILED","La exportación de usuarios falló...");
+define("_USER_EXPORT_COMPLETED","Exportación de Usuarios completa");
+define("_G2_NEXT_PAGE","Siguiente Página");
+define("_PHPEMBEDFILE_ERROR","Primero necesitas instalar y configurar Galley2. Luego esta celda deberá indicar el directorio raiz en el cual se encuetra Galley 2");
+define("_SHOWSIDEBAR","Mostrar barra de desplazamiento (No marcar si utiliza el Bloque G2 Sidebar)");
+
+define("_G2_EMBED_PHP_FILE","embed.php");
+define("_G2_CONFIGURATION_NOT_DONE","Este módulo no ha sido configurado aún");
+
+?>
