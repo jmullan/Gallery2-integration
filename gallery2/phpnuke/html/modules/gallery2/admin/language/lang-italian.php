13bf566107fdc85bf07c808d6fba2dd49e3c4307
Dariush Molavi <dariush.molavi@L-3Com.com>
2005-09-22 11:42:35 +0000

    * New: Italian language files. Thanks to Emanuele De Angelis.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/integration@11660 57fcd75e-5312-0410-8df3-f5eb6fbb1595

---
+++ gallery2/phpnuke/html/modules/gallery2/admin/language/lang-italian.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * $RCSfile$
+ *
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2005 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+/**
+ * Gallery 2 language file for PHPNuke.
+ * @version $Revision$ $Date$
+ * @author Dariush Molavi <dari@nukedgallery.net>
+ */
+
+
+define("MOD_NAME","gallery2");
+
+define("_PHPEMBEDFILE","Path completo della directory di Gallery2");
+define("_EMBEDURI","URL della tua embedded Gallery");
+define("_RELATIVEG2PATH","Path relativo della directory di Gallery2");
+define("_LOGINREDIRECT","URL per il login");
+define("_ACTIVEUSERID","Attivare User ID");
+define("_UPDATEEMBEDSETTINGSG2","Verifica dell'installazione-update di Gallery2");
+define("_UPDATEMAINSETTINGSG2","Update settaggi");
+define("_G2USEREXPORT","Export degli utenti PhpNuke in Gallery 2");
+define("_G2_ERROR","Errore");
+define("_NOTWRITABLE","non è scrivibile");
+define("_CFG_UPDATED","File di configurazione aggiornato");
+define("_USER_EXPORT_UPDATED","Utenti sincornizzati");
+define("_USER_EXPORT_FAILED","Export degli utenti fallito...");
+define("_USER_EXPORT_COMPLETED","Export totale eseguito");
+define("_G2_NEXT_PAGE","Prossima pagina");
+define("_PHPEMBEDFILE_ERROR","Tu hai bisogno di installare e configurare prima G2. Questo campo deve puntare alla directory dove è installato G2.");
+define("_SHOWSIDEBAR","Visualizza la barra (Uncheck se vuoi usare la barra)");
+
+define("_G2_EMBED_PHP_FILE","embed.php");
+define("_G2_CONFIGURATION_NOT_DONE","Il modulo non è configurato!");
+
+?>
