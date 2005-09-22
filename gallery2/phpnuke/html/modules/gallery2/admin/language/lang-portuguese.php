8df85a6e521ca67e537d22d630f8d7ff93f24030
Dariush Molavi <dariush.molavi@L-3Com.com>
2005-09-22 16:52:40 +0000

    * New: Portuguese language files. Thanks to netmen.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/integration@11665 57fcd75e-5312-0410-8df3-f5eb6fbb1595

---
+++ gallery2/phpnuke/html/modules/gallery2/admin/language/lang-portuguese.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * $RCSfile$
+ *
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2005 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+/*
+ * Ficheiro de linguagem portuguesa do modulo Gallery 2 para o PhpNuke. 
+ * @version $Revision$ $Date$ 
+ * @author Dariush Molavi <dari@nukedgallery.net> 
+ */ 
+
+define("_PHPEMBEDFILE","Caminho Completo para o directorio do Gallery2"); 
+define("_EMBEDURI","URL para a tua embedded Gallery"); 
+define("_RELATIVEG2PATH","O caminho relativo para o directorio da tua Gallery2"); 
+define("_LOGINREDIRECT","URL de login"); 
+define("_ACTIVEUSERID","Activar User ID"); 
+define("_UPDATEEMBEDSETTINGSG2","Verificar as settings da instalação ou update da tua Gallery2 "); 
+define("_UPDATEMAINSETTINGSG2","Actualizar settings principais"); 
+define("_G2USEREXPORT","Exportar os utilizadores PhpNuke para a Gallery 2"); 
+define("_G2_ERROR","Erro"); 
+define("_NOTWRITABLE","Não permite a escrita"); 
+define("_CFG_UPDATED","Ficheiro de configuração actualizado "); 
+define("_USER_EXPORT_UPDATED","Utilizadores estão sincronizados"); 
+define("_USER_EXPORT_FAILED","Falhou o export dos utilziadores ..."); 
+define("_USER_EXPORT_COMPLETED","Export de utilizadores completo"); 
+define("_G2_NEXT_PAGE","Continua para a paginar seguinte"); 
+define("_PHPEMBEDFILE_ERROR","Precisas de instalar e configurar a G2 primeiro. Depois disso, este campo precisa de apontar para a root do Gallery2."); 
+define("_SHOWSIDEBAR","Mostrar barra lateriar (Não selecionar se estiveres a usar o block lateral do Gallery2)"); 
+
+define("_G2_EMBED_PHP_FILE","embed.php"); 
+define("_G2_CONFIGURATION_NOT_DONE","Este modulo ainda não foi configurado.");
+
+?>
