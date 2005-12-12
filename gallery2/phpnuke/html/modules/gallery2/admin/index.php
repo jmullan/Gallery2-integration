548416a74f12374ce4a111649366cc162e7a015d
Dariush Molavi <dariush.molavi@L-3Com.com>
2005-12-12 13:41:28 +0000

    Forgot to upgrade rev in db
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/integration@12266 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery2/phpnuke/html/modules/gallery2/admin/index.php
+++ gallery2/phpnuke/html/modules/gallery2/admin/index.php
@@ -28,7 +28,7 @@
 
 global $prefix, $db, $g2config_error, $currentlang, $admin_file, $module_name;
 
-$embedVersion = "0.4.1";
+$embedVersion = "0.4.2";
 
 if(!isset($admin_file)) {
 	$admin_file = "admin";
